4081fc21b349223332843b96934d5d9df60a2ad9
dargonmangfu <mn23xm@leeds.ac.uk>
2025-07-01 14:44:38 +0800

    MNIST数据集

---
+++ MNIST.py
@@ -0,0 +1,12 @@
+
+class MNIST():
+    def __init__(self, data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+
+    def preprocess(self):
+        # Example preprocessing step
+        self.data = self.data / 255.0  # Normalize pixel values
+        return self.data
