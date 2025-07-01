c2ac9194a46f3cdccbbaa207e6f3d44a095c4bb5
dargonmangfu <mn23xm@leeds.ac.uk>
2025-07-01 15:11:19 +0800

    设置内置变量

--- MNIST.py
+++ MNIST.py
@@ -1,9 +1,13 @@
 import torch
 import torchvision
+from torchvision import transforms
+from torch.utils import data
 
 class MNIST():
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+        self.trans = transforms.ToTensor()
+        self.mnist_train = torchvision.datasets.FashionMNIST(root='./data', train=True, transform=self.trans ,download=True)
+        self.mnist_test = torchvision.datasets.FashionMNIST(root='./data', train=False, transform=self.trans ,download=True)
 
     def get_data(self):
         return self.data
